f06ff798aeaf3e55a9f3dd09ace233ecd4e87834
amin <mohaddes.amin@gmail.com>
2023-03-18 14:17:55 +0330

    add project

---
+++ WebApi/Models/StoreProcedure_Model.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class StoreProcedure_Model
+    {
+        [Required]
+        public string Database { get; set; } = "";
+        [Required]
+        public string StoreProcedure_Name { get; set; } = "";
+        
+        public List<Parameter_Model>? parameter_Models { get; set; }
+    }
+}
